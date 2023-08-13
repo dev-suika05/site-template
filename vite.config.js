7ed3daa7a642112ca20ef443c3ddf8920c7a19be
suika05 <dev.suika05@gmail.com>
2023-08-13 14:00:03 +0900

    HTMLをハンドルバー化、HTMLを複数出力できるよう変更

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+import handlebars from 'vite-plugin-handlebars';
+import fs from 'fs';
+
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
+
+const fileNameList = fs.readdirSync(resolve(__dirname, './src/'));
+const htmlFileList = fileNameList.filter(file => /.html$/.test(file));
+const inputFiles = {};
+for (let i = 0; i < htmlFileList.length; i++) {
+  const file = htmlFileList[i];
+  inputFiles[file.slice(0,-5)] = resolve(__dirname, './src/' + file );
+}
+
+const pageData = {
+  '/index.html': {
+    isHome: true,
+    title: 'TOP',
+    directory: '.',
+  },
+  '/sample.html': {
+    isHome: false,
+    title: 'Sample Page',
+    directory: '.'
+  },
+  '/detail/index.html': {
+    isHome: false,
+    title: 'Detail',
+    directory: '../..'
+  },
+};
 
 export default defineConfig({
 	base: './',
@@ -10,16 +39,21 @@ export default defineConfig({
 	build: {
 		outDir,
 		rollupOptions: {
-			// input: {
-			// 	sample1: resolve(root, 'sample1', 'index.html'),
-			// 	// ディレクトリ名: resolve(root, 'ディレクトリ名', 'index.html'),
-			// },
+			input: inputFiles,
 			output: {
 				entryFileNames: `assets/js/script.js`,
 				assetFileNames: `assets/css/style.[ext]`,
 			}
 		},
 	},
+	plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, './src/inc'),
+      context(pagePath) {
+        return pageData[pagePath];
+      },
+    }),
+  ],
 	server: {
 		port: 8080
 	}
